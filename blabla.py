234ac6303a72bbb0cd5872819d66e76f6bbc7e6c
miz4el <mizael.ilha@gmail.com>
2025-10-21 19:51:39 -0300

    primeiro commit

---
+++ blabla.py
@@ -0,0 +1,7 @@
+import html 
+
+x= int(input("Digite um numero inteiro: "))
+
+while(x==4):
+    x+1
+    print(x)
--- desafio3.py
+++ desafio3.py
@@ -4,4 +4,4 @@ mês= input("qual foi o mês da sua ultima compra? ")
 valor= int(input("Qual foi o valor da sua compra? "))
 desconto= nome+"10"
 
-print(f"ola {nome} {sobrenome},a quanto tempo heim?, como vai você? ficamos felizes em te ver novamente aqui, puxando em nossos dados checamos que a sua ultima compra no valor de {valor} no mês de: {mês}, correto ?, para melhorar o seu dia lhe daremos um cupom de 10%, que tal aproveitar !? se for de seu interesse é seu utilizar o seu cupom {desconto} ")
+print(f"ola {nome} {sobrenome},a quanto tempo heim?, como vai você? ficamos felizes em te ver novamente aqui, puxando em nossos dados checamos que a sua ultima compra no valor de {valor:.2f} no mês de: {mês}, correto ?, para melhorar o seu dia lhe daremos um cupom de 10%, que tal aproveitar !? se for de seu interesse é seu utilizar o seu cupom {desconto} ")
