7abac56763787ad1d4f4aa98a694bd0322cb138d
miz4el <mizael.ilha@gmail.com>
2025-11-03 19:32:39 -0300

    comitindo crimes

--- venv/CEP.py
+++ venv/CEP.py
@@ -1,15 +1,13 @@
 import requests
 #IMPORTANTE A BIBLIOTECA REQUESTS
 
-integrantesx = {
+integrantes = {
     "Dione": "11633422",
     "Kaio": "11633010",
     "Cecilia": "04013001",
     "Mizael": "11630087"
 }
-integrantes = {
-    "Dante": "Danila"
-}
+
 #DEFININDO A LISTA (CHAVES "NOMES" E VALORES "CEPS").
 
 for nome, cep in integrantes.items():
@@ -21,10 +19,12 @@ for nome, cep in integrantes.items():
         dados = resposta.json()
         cidade = dados.get("localidade", "cidade não encontrada")
         print(f"{nome} mora em {cidade}!!")
-        #SE A REPOSTA FOR 200 (POSITIVA), EU PEGO A RESPOSTA EM JSON USANDO A VARIAVEL DADOS
-        #USO A VARIAVEL CIDADE PARA USAR O GET NOS DADOS PARA PEGAR OS ITENS "LOCALIDADE" CASO POSITIVO, CASO NEGATIVO RETORNAR "CIDADE NÃO ENCONTRADA".
-        #SE POSITIVO A LOCALIDADE VAI ESTAR NA VARIAVEL CIDADE
-        #COM ISSO USAREMOS O PRINT PARA EXIBIR NA TELA AS INFORMAÇÕES "NOME" E "CIDADE"
+        '''
+        SE A REPOSTA FOR 200 (POSITIVA), EU PEGO A RESPOSTA EM JSON USANDO A VARIAVEL DADOS
+        USO A VARIAVEL CIDADE PARA USAR O GET NOS DADOS PARA PEGAR OS ITENS "LOCALIDADE" CASO POSITIVO, CASO NEGATIVO RETORNAR "CIDADE NÃO ENCONTRADA".
+        SE POSITIVO A LOCALIDADE VAI ESTAR NA VARIAVEL CIDADE
+        COM ISSO USAREMOS O PRINT PARA EXIBIR NA TELA AS INFORMAÇÕES "NOME" E "CIDADE"
+        '''
 
     else: 
         print(f"{nome}, Dados não encontrados!")    
